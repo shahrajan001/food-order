1b268a134a4161577a841beaaf2437b01590c6d4
rajan shah <abhishek.desai@appgambit.com>
2022-11-01 14:09:12 +0530

    Animation to the cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,34 @@
-import React,{useContext} from "react";
+import React,{useContext,useEffect,useState} from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from './../../store/cart-context'
 import styles from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props)=>{
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartContext = useContext(CartContext)
     const numberOfCartItems = cartContext.items.reduce((current,item)=>
     { return current + item.amount},0)
 
-    return <button className={styles.button} onClick={props.onClick}>
+    const { items } = cartContext;
+
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+      if (items.length === 0) {
+        return;
+      }
+      setBtnIsHighlighted(true);
+  
+      const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+  
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={styles.icon}> 
             <CartIcon/>
         </span>
