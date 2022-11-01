14dd171dfd135de0ead0b946d5cfaee5feffe1b4
rajan shah <abhishek.desai@appgambit.com>
2022-11-01 11:28:58 +0530

    modified usereducer logic for cart items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,27 @@ const defaultCartState = {
 
 const CartReducer = (state, action) => {
     if (action.type === 'ADD_CART') {
-        const updatedItems = state.items.concat(action.item)
-
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
+        const existingCartItemIndex = state.items.findIndex((element) => {
+            return element.id === action.item.id
+        })
+        const existingCartItem = state.items[existingCartItemIndex]
+
+        let updatedItem
+        let updatedItems
+        // const updatedItems = state.items.concat(action.item)
+        if(existingCartItem){
+            updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }else{
+            updatedItems = state.items.concat(action.item)
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
