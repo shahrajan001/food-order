64a5297c16a614987bca38da012336aa849771e3
rajan shah <abhishek.desai@appgambit.com>
2022-11-18 11:49:52 +0530

    Submitting the order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
 import styles from "./Cart.module.css"
 import CartItem from './CartItem'
 import Modal from "../UI/Modal/Modal";
@@ -7,21 +8,45 @@ import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
     const [isOrdering, setIsOrdering] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [didSubmit, setDidSubmit] = useState(false)
     const cartContext = useContext(CartContext)
 
     const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`
     const cartIsEmpty = !cartContext.items.length > 0
 
     const cartItemAddHandler = (item) => {
-        cartContext.addItem({...item,amount:1})
+        cartContext.addItem({ ...item, amount: 1 })
     }
 
     const cartItemRemoveHandler = (id) => {
         cartContext.removeItem(id)
     }
 
-    const orderHandler = ()=>{
+    const orderHandler = () => {
         setIsOrdering(true)
+    }
+
+    const submitOrderHandler = async (userData)=>{
+        setIsSubmitting(true)
+            var config = {
+              method: 'post',
+              url: 'https://food-order-b13ba-default-rtdb.firebaseio.com/orders.json',
+              data: JSON.stringify({
+                user:userData,
+                orderedItems:cartContext.items}),
+              headers:{'content-type': 'application/json'}
+            };
+            
+            axios(config)
+            .then(function (response) {
+            //   console.log(JSON.stringify(response.data));
+              setDidSubmit(true)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });    
+            setIsSubmitting(false)    
     }
 
     const cartItems = <ul className={styles['cart-items']}>{
@@ -36,17 +61,26 @@ const Cart = (props) => {
         })}
     </ul>
 
-    return <Modal onClose={props.onClose}>
+    const cartModalContent = <React.Fragment>
         {!isOrdering && cartItems}
         <div className={styles.total}>
             <span> Total Amount </span>
             <span> {totalAmount} </span>
         </div>
-       {isOrdering &&  <Checkout onClose ={props.onClose}/>}
+        {isOrdering && <Checkout onClose={props.onClose} onConfirm={submitOrderHandler} />}
         <div className={styles.actions}>
-           {!isOrdering && <button className={styles['button--alt']} onClick={props.onClose}> Close </button>}
-            {!isOrdering && !cartIsEmpty && <button className={styles.button} onClick = {orderHandler}> Order </button>}
+            {!isOrdering && <button className={styles['button--alt']} onClick={props.onClose}> Close </button>}
+            {!isOrdering && !cartIsEmpty && <button className={styles.button} onClick={orderHandler}> Order </button>}
         </div>
+    </React.Fragment>
+
+    const isSubmittingModalContent = <p> Sending order Data</p>
+    const didSubmitModalContent = <p> Your order will be delivered in next 30 minutes.</p>
+
+    return <Modal onClose={props.onClose}>
+        {!isSubmitting && !didSubmit && cartModalContent}
+        {isSubmitting && isSubmittingModalContent}
+        {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
 }
 
